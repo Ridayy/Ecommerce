64dd81a9e08cff1c900c0ac6c02c1af1d3840612
Rida Arif <ridaarif20@gmail.com>
2019-12-30 23:53:50 +0500

    sql added

--- ecommerce.sql
+++ ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 30, 2019 at 03:53 PM
+-- Generation Time: Dec 30, 2019 at 07:52 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.1.33
 
